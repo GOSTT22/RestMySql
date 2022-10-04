a080544f321527d3a149542f3f460f203f9745a6
Stanislav <bloger.tayzle@gmail.com>
2022-10-04 21:15:33 +0300

    Change Title27

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'MyExpress' });
+  res.render('index', { title: 'MyExpress22' });
 });
 
 module.exports = router;
